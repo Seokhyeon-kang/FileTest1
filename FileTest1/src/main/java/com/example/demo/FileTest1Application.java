20ddab005280d1e983eacb1ac09ca0d5930493a0
User <User@DESKTOP-ML7EMSF>
2022-12-31 10:47:07 +0900

    test

---
+++ FileTest1/src/main/java/com/example/demo/FileTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTest1Application.class, args);
+	}
+
+}
